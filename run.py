9d063bc9c894b3c04b7ec07ff3774c581c71841a
makena-s <48397225+makena-s@users.noreply.github.com>
2020-08-27 12:39:12 +1200

    set up archive page

--- run.py
+++ run.py
@@ -7,11 +7,11 @@ DOGDB = 'sqlite/dog.db'
 
 def fetchData(con):
     db = sqlite3.connect(DOGDB) # connect to database
+    
     comments = []
-    cur = db.execute('SELECT * FROM comments')
+    cur = db.execute('SELECT * FROM comments ORDER BY timestamp DESC')
     for row in cur:
         comments.append(list(row))
-        
     newest = []
     cur = db.execute('SELECT * FROM comments ORDER BY timestamp DESC LIMIT 3')
     for row in cur:
@@ -27,17 +27,26 @@ def index():
     
     print('hello world ;^)')
     print(sqlite3.connect(DOGDB)) # print in command prompt that connnection has worked
-    
     return render_template('index.html',
                            disclaimer='disclaimer, babey!',
+                           newest=data['newest']
+                          )
+
+@app.route('/archive')
+def archive():
+    con = sqlite3.connect(DOGDB)
+    data = fetchData(con)
+    con.close()
+    return render_template('archive.html',
+                           disclaimer='appraise dog.',
                            comments=data['comments'],
-                           
-                           newest=data['newest'
-                           ]
+                           newest=data['newest']
                           )
 
 
-
+@app.errorhandler(404) # custom 404 page
+def page_not_found(e):
+    return render_template('404.html', disclaimer='good gravy!'), 404
 
 
 
